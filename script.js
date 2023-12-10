1a531a917e6f48079e990f1d8eaddad0672a7d71
nwacrypto <98532800+nwacrypto@users.noreply.github.com>
2023-12-10 20:29:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Fetch JSON data
+    let jsonData;
+
+    // Fetch JSON data when the page is loaded
     fetch("data.json")
         .then(response => response.json())
         .then(data => {
-            // Add event listener to the search input
-            document.getElementById("searchInput").addEventListener("input", function () {
-                const searchTerm = this.value.toLowerCase();
-                const searchResults = document.getElementById("search-results");
-                searchResults.innerHTML = ""; // Clear previous results
+            jsonData = data;
 
-                // Filter data based on the search term
-                const filteredData = data.filter(item =>
-                    item.title.toLowerCase().includes(searchTerm) ||
-                    item.description.toLowerCase().includes(searchTerm)
-                );
-
-                // Display results
-                filteredData.forEach(item => {
-                    const listItem = document.createElement("li");
-                    listItem.classList.add("result-item");
-                    listItem.innerHTML = `<strong>${item.title}</strong><br>${item.description}`;
-                    searchResults.appendChild(listItem);
-                });
+            // Add event listeners
+            document.getElementById("searchButton").addEventListener("click", performSearch);
+            document.getElementById("searchInput").addEventListener("keydown", function (event) {
+                if (event.key === "Enter") {
+                    performSearch();
+                }
             });
         })
         .catch(error => console.error("Error fetching JSON data:", error));
+
+    function performSearch() {
+        const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+        const searchResults = document.getElementById("search-results");
+        searchResults.innerHTML = ""; // Clear previous results
+
+        // Filter data based on the search term
+        const filteredData = jsonData.filter(item =>
+            item.title.toLowerCase().includes(searchTerm) ||
+            item.description.toLowerCase().includes(searchTerm)
+        );
+
+        // Display results
+        filteredData.forEach(item => {
+            const listItem = document.createElement("li");
+            listItem.classList.add("result-item");
+            listItem.innerHTML = `<strong>${item.title}</strong><br>${item.description}`;
+            searchResults.appendChild(listItem);
+        });
+    }
 });
